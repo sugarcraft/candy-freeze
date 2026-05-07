3c405ce9fa79a52118afbbdbc80942b43a95a085
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Korean translations for candy-freeze.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.unknown_flag'  => "candyfreeze: 인식할 수 없는 플래그 '{flag}'",
+    'cli.unknown_theme' => "candyfreeze: 알 수 없는 테마 '{name}'. 알려진 테마: dark, light, dracula, tokyo-night, nord",
+    'cli.read_failed'   => 'candyfreeze: 입력 읽기 실패',
+    'cli.write_failed'  => "candyfreeze: 쓰기 실패 '{path}'",
+];
